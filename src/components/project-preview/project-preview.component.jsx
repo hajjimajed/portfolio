8b1f6a7c0cf48d2e95b48caea1bc3a223321ede0
hajjimajed <hajjimajed78@gmail.com>
2023-02-21 12:07:50 +0100

    finishin project preview

---
+++ src/components/project-preview/project-preview.component.jsx
@@ -0,0 +1,21 @@
+import './project-preview.styles.scss'
+import { Link } from 'react-router-dom';
+import { ReactComponent as Arrow } from '../../assets/right-arrow.svg'
+
+const ProjectPreview = () => {
+    return (
+        <div className='project-preview-container'>
+            <div className='project-info'>
+                <h1>project name<span>&#9632;</span></h1>
+                <p>project genre</p>
+                <p>project info</p>
+                <Link className='project-link'><h1>view details</h1><Arrow className='link-arrow' /></Link>
+            </div>
+            <div className='project-image'>
+                <img src="https://i.pinimg.com/originals/aa/bb/bc/aabbbc70a1a36e57ec3fb461a954ce16.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default ProjectPreview;
--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -1,8 +1,23 @@
 import './projects.styles.scss'
 
+import ProjectPreview from '../../components/project-preview/project-preview.component';
 
 const Projects = () => {
+    return (
+        <div className='projects-container'>
+            <div className='projects-header'>
+                <h1>projects<span>&#9632;</span></h1>
+                <div className='projects-header-intro'>
+                    <div></div>
+                    <h1>see my work</h1>
+                    <div></div>
+                </div>
+            </div>
 
+            <ProjectPreview />
+
+        </div>
+    )
 }
 
 export default Projects;

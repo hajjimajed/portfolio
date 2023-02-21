f124f61c439252d220b219c036b8880b7cb1a117
hajjimajed <hajjimajed78@gmail.com>
2023-02-21 17:11:11 +0100

    adding splash screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
@@ -7,16 +8,36 @@ import Contact from './routes/contact/contact.component';
 import About from './routes/about/about.component';
 import Projects from './routes/projects/projects.component';
 
+import Loader from './components/loader/loader.component';
+
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+
+      {isLoading ? <Loader /> :
+        <Routes>
+          <Route path='/' element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+          </Route>
+        </Routes>
+      }
+
+
+    </>
   );
 }
 
